841e0fe585887f6f20f4569a622a203e6ec17678
Dennis Padilla <d_padilla.y@hotmail.com>
2023-07-13 13:28:52 -0400

    Added functionality to maintain data on screen rotation

---
+++ app/src/main/java/com/example/contactsapp/ContactViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.contactsapp
+
+import androidx.lifecycle.ViewModel
+
+class ContactViewModel : ViewModel() {
+    val contacts: ArrayList<String> = ArrayList()
+}
--- app/src/main/java/com/example/contactsapp/MainActivity.kt
+++ app/src/main/java/com/example/contactsapp/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.contactsapp
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.contactsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var contactAdapter: ArrayAdapter<String>
-    private lateinit var contacts: ArrayList<String>
+    private lateinit var viewModel: ContactViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +18,26 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        contacts = ArrayList()
-        contactAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contacts)
-        binding.listViewContacts.adapter = contactAdapter
+        viewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
+
+        // Initialize the contactAdapter only if it is null
+        if (!::contactAdapter.isInitialized) {
+            contactAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, viewModel.contacts)
+            binding.listViewContacts.adapter = contactAdapter
+        }
 
         binding.buttonAddContact.setOnClickListener {
             val name = binding.editTextName.text.toString()
             val phone = binding.editTextPhone.text.toString()
             val contact = "$name - $phone"
 
-            contacts.add(contact)
+            viewModel.contacts.add(contact)
             contactAdapter.notifyDataSetChanged()
             clearFields()
         }
 
         binding.listViewContacts.setOnItemClickListener { _, _, position, _ ->
-            val contact = contacts[position]
+            val contact = viewModel.contacts[position]
             val name = contact.split(" - ")[0]
             val phone = contact.split(" - ")[1]
 
@@ -41,7 +46,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.listViewContacts.setOnItemLongClickListener { _, _, position, _ ->
-            contacts.removeAt(position)
+            viewModel.contacts.removeAt(position)
             contactAdapter.notifyDataSetChanged()
             clearFields()
             true
