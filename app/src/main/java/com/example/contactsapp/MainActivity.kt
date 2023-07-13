c9d135ba417bfac94faa942977a4be7469d8c5ba
Dennis Padilla <d_padilla.y@hotmail.com>
2023-07-13 13:17:32 -0400

    Incorporated the binding data

--- app/src/main/java/com/example/contactsapp/MainActivity.kt
+++ app/src/main/java/com/example/contactsapp/MainActivity.kt
@@ -1,37 +1,29 @@
 package com.example.contactsapp
 
-
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.contactsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var editTextName: EditText
-    private lateinit var editTextPhone: EditText
-    private lateinit var buttonAddContact: Button
-    private lateinit var listViewContacts: ListView
+    private lateinit var binding: ActivityMainBinding
     private lateinit var contactAdapter: ArrayAdapter<String>
     private lateinit var contacts: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        editTextName = findViewById(R.id.editTextName)
-        editTextPhone = findViewById(R.id.editTextPhone)
-        buttonAddContact = findViewById(R.id.buttonAddContact)
-        listViewContacts = findViewById(R.id.listViewContacts)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         contacts = ArrayList()
         contactAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contacts)
-        listViewContacts.adapter = contactAdapter
+        binding.listViewContacts.adapter = contactAdapter
 
-        buttonAddContact.setOnClickListener {
-            val name = editTextName.text.toString()
-            val phone = editTextPhone.text.toString()
+        binding.buttonAddContact.setOnClickListener {
+            val name = binding.editTextName.text.toString()
+            val phone = binding.editTextPhone.text.toString()
             val contact = "$name - $phone"
 
             contacts.add(contact)
@@ -39,16 +31,16 @@ class MainActivity : AppCompatActivity() {
             clearFields()
         }
 
-        listViewContacts.setOnItemClickListener { _, _, position, _ ->
+        binding.listViewContacts.setOnItemClickListener { _, _, position, _ ->
             val contact = contacts[position]
             val name = contact.split(" - ")[0]
             val phone = contact.split(" - ")[1]
 
-            editTextName.setText(name)
-            editTextPhone.setText(phone)
+            binding.editTextName.setText(name)
+            binding.editTextPhone.setText(phone)
         }
 
-        listViewContacts.setOnItemLongClickListener { _, _, position, _ ->
+        binding.listViewContacts.setOnItemLongClickListener { _, _, position, _ ->
             contacts.removeAt(position)
             contactAdapter.notifyDataSetChanged()
             clearFields()
@@ -57,7 +49,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun clearFields() {
-        editTextName.text.clear()
-        editTextPhone.text.clear()
+        binding.editTextName.text.clear()
+        binding.editTextPhone.text.clear()
     }
 }
