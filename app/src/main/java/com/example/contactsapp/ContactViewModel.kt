5908e741eb7ad81843ecae0cf12ae908b0b59e30
Dennis Padilla <d_padilla.y@hotmail.com>
2023-08-08 17:53:15 -0400

    Modified ContactViewModel to implement database

--- app/src/main/java/com/example/contactsapp/ContactViewModel.kt
+++ app/src/main/java/com/example/contactsapp/ContactViewModel.kt
@@ -1,10 +1,35 @@
 package com.example.contactsapp
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * ViewModel class that holds the contact data.
  */
-class ContactViewModel : ViewModel() {
-    val contacts: ArrayList<Contact> = ArrayList()
+class ContactViewModel(application: Application) : AndroidViewModel(application) {
+    private val dao: ContactDao = AppDatabase.getDatabase(application).contactDao()
+
+    val contacts: LiveData<List<Contact>> = dao.getAllContacts()
+
+    fun addOrUpdateContact(contact: Contact) {
+        viewModelScope.launch(Dispatchers.IO) { // Use Dispatchers.IO for database operations
+            if (contact.id <= 0) {
+                dao.insert(contact)
+            } else {
+                dao.update(contact)
+            }
+        }
+    }
+
+    fun deleteContact(contact: Contact) {
+        viewModelScope.launch(Dispatchers.IO) { // Use Dispatchers.IO for database operations
+            dao.delete(contact)
+        }
+    }
+
 }
